c0c2b00f3370bae4d76ace66152e6773d55393a8
xavierflandro <xavierflandro@gmail.com>
2021-03-16 21:31:53 -0600

    Initial Homepage and Controller work

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -12,16 +12,46 @@ namespace ProjectOne.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        //private FormContext _context { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        /*public HomeController(ILogger<HomeController> logger, FormContext context)
         {
             _logger = logger;
-        }
+            _context = context;
+
+        }*/
 
         public IActionResult Index()
         {
             return View();
         }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        /*[HttpPost]
+        public IActionResult SignUp(TimePeriod time)
+        {
+            return View("Form", time);
+        }*/
+
+        /*[HttpPost]
+        public IActionResult Form(Appointment appt)
+        {
+            if (ModelState.IsValid)
+            {
+                //Update and save database
+                _context.Appointments.Add(appt);
+                _context.SaveChanges();
+
+                return View("Index");
+            }
+
+            return View();
+        }*/
 
         public IActionResult Privacy()
         {
