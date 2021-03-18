54be02480a91fca9e7a17811013724e48fd5f7ce
xavierflandro <xavierflandro@gmail.com>
2021-03-18 12:23:53 -0600

    adding in some comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace ProjectOne.Controllers
         private readonly ILogger<HomeController> _logger;
         private AppointmentListContext _context { get; set; }
 
+        //set private context object in constructor to be used throughout controller 
         public HomeController(ILogger<HomeController> logger, AppointmentListContext context)
         {
             _logger = logger;
@@ -45,6 +46,7 @@ namespace ProjectOne.Controllers
         [HttpPost]
         public IActionResult Form(string slotDay, string slotTime)
         {
+            //create new appointment object and set the datetime attribute using the strings from previous form
             var appt = new Appointment
             {
                 Datetime = slotDay + " " + slotTime + ":00"
@@ -70,6 +72,7 @@ namespace ProjectOne.Controllers
                 _context.Appointments.Add(appt);
                 _context.SaveChanges();
 
+                //Route back to home
                 return View("Index");
             }
 
